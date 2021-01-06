437f56c8fd9c4925258394427dc028f1fb8af73e
Shivz3232 <dgshivu3232@gmail.com>
2021-01-06 12:18:41 +0530

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+ *   Primary file for the API
+ */
+
+//  Dependencies
+ const server = require('./lib/server');
+ const workers = require('./lib/workers');
+
+//  Declare the app
+ const app = {};
+
+//  Init function
+app.init = function() {
+    // Start the server
+    server.init();
+
+    // Start the workers
+    workers.init();
+}
+
+// Execute
+app.init()
+
+// Export the app
+module.exports = app;
