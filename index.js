9bee4ae3fefc057aa795ce09354abfee1e026c29
Shyvu <dgshivu3232@gmail.com>
2021-01-29 14:35:55 +0530

    idk what happened

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 //  Dependencies
  const server = require('./lib/server');
  const workers = require('./lib/workers');
+ const cli = require('./lib/cli');
 
 //  Declare the app
  const app = {};
@@ -16,6 +17,11 @@ app.init = function() {
 
     // Start the workers
     workers.init();
+
+    // Start the CLI, but make sure it starts last
+    setTimeout(function() {
+        cli.init();
+    }, 50);
 }
 
 // Execute
